cf44783336c89b74ca7eb81d99051791b34e888f
Derek Colley <derek@metaspan.com>
2024-01-15 18:22:40 +0000

    deactivate dock for the moment

--- vue3/src/store/modules/asset.ts
+++ vue3/src/store/modules/asset.ts
@@ -53,7 +53,7 @@ const chains = [
 
 
   { id: 'debio', type: 'crypto', name: 'DeBio', active: false, parent: '', parachain: false, code: '', logo: 'images/logos/chains/debio.svg' },
-  { id: 'dock', type: 'crypto', name: 'Dock', active: true, parent: '', parachain: false, code: 'DOCK', decimals: 6, logo: 'images/logos/nodes/dock-mainnet.png' },
+  { id: 'dock', type: 'crypto', name: 'Dock', active: false, parent: '', parachain: false, code: 'DOCK', decimals: 6, logo: 'images/logos/nodes/dock-mainnet.png' },
 
 
 
