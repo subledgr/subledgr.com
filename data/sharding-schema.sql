3e21bbb4511cdcc8a07c68aea83c958aad57c583
Derek Colley <derek@colley.cc>
2024-01-24 14:27:16 +0100

    Add shard for DOCK

--- data/sharding-schema.sql
+++ data/sharding-schema.sql
@@ -84,3 +84,18 @@ CREATE FOREIGN TABLE IF NOT EXISTS transfer_polkadot PARTITION OF transfer
   FOR VALUES IN ('polkadot')
   SERVER shard_polkadot
   OPTIONS (table_name 'transfer');
+
+-- DOCK
+
+-- DROP SERVER IF EXISTS shard_dock;
+CREATE SERVER shard_dock FOREIGN DATA WRAPPER postgres_fdw
+  OPTIONS (dbname 'subledgr_subsquid_dock', host 'localhost', port '5432');
+
+-- also for postgres!
+CREATE USER MAPPING for subledgr_subsquid SERVER shard_dock 
+  OPTIONS (user 'subledgr_subsquid', password 'subledgr_subsquid');
+
+CREATE FOREIGN TABLE IF NOT EXISTS transfer_dock PARTITION OF transfer
+  FOR VALUES IN ('dock')
+  SERVER shard_dock
+  OPTIONS (table_name 'transfer');
